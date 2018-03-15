8aee57808bddc510be0605fd283f16e05229df06
Bibek Moulik <36233718+bibekmoulik@users.noreply.github.com>
2018-03-15 19:50:31 +0530

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,174 @@
+var photoIndex = 5;
+var projectPath = "";
+
+function showDescription()
+{
+	var imagePath = projectPath + "\\" + document.getElementById("largeScreen").src;
+	var file = readFile(imagePath);
+	if (file && file.name) 
+	{
+		EXIF.getData(file, function(){
+			//document.getElementById("descriptionSpan").innerText = EXIF.getTag(file, "ImageDescription");
+			alert(EXIF.getTag(file, "ImageDescription"));
+		});
+	}
+}
+
+function funcLoad()
+{
+	container = document.getElementById("container");
+	
+	while(parseInt(photoIndex) > 0)
+	{
+		var id = pad(photoIndex,10);
+		var srcPath = "images/" + id + ".jpg";
+		var image = document.createElement("img");
+		image.name = "photo";
+		image.id = id;
+		image.src = srcPath;
+		image.setAttribute("onclick","zoomIn('" + srcPath + "','" + id + "');");
+		image.className = "w3-thumbnail-Img";
+		container.appendChild(image);
+		photoIndex = photoIndex - 1;
+	}
+}
+
+function pad(str,max)
+{
+	str = str.toString();
+	return str.length < max ? pad("0" + str, max) : str;
+}
+
+function zoomIn(photoSrc,slideIndex)
+{
+	document.getElementById("modal01").style.display = "block";
+	document.getElementById("largeScreen").src = photoSrc;
+	photoIndex = parseInt(slideIndex);
+}
+
+function plusDivs(n)
+{
+	photoIndex = photoIndex + n;
+	var photos = document.getElementsByName("photo");
+	
+	if (photoIndex > photos.length)
+	{
+		photoIndex = 1;
+	}
+	if (photoIndex < 1)
+	{
+		photoIndex = photos.length
+	}
+	var arrIndex = photos.length - photoIndex;
+	document.getElementById("largeScreen").src = photos[arrIndex].src;
+}
+
+function closeScreen()
+{
+	document.getElementById("modal01").style.display = "none";
+}
+
+function createXMLHttpObject()
+{
+	var xmlhttp=false;
+	/* running locally on IE5.5, IE6, IE7 */
+	if(location.protocol=="file:")
+	{
+		if(!xmlhttp)
+		{
+			try
+			{
+				xmlhttp=new ActiveXObject("MSXML2.XMLHTTP");
+			}
+			catch(e)
+			{
+				xmlhttp=false;
+			}
+		}
+		if(!xmlhttp)
+		{
+			try
+			{
+				xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			catch(e)
+			{
+				xmlhttp=false;
+			}
+		}
+	}
+	/* IE7, Firefox, Safari, Opera...  */
+	if(!xmlhttp)
+	{
+		try
+		{
+			xmlhttp=new XMLHttpRequest();
+		}
+		catch(e)
+		{
+			xmlhttp=false;
+		}
+	}
+	/* IE6 */
+	if(typeof ActiveXObject != "undefined")
+	{
+		if(!xmlhttp)
+		{
+			try
+			{
+				xmlhttp=new ActiveXObject("MSXML2.XMLHTTP");
+			}
+			catch(e)
+			{
+				xmlhttp=false;
+			}
+		}
+		if(!xmlhttp)
+		{
+			try
+			{
+				xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+			}
+			catch(e)
+			{
+				xmlhttp=false;
+			}
+		}
+	}
+	/* IceBrowser */
+	if(!xmlhttp)
+	{
+		try
+		{
+			xmlhttp=createRequest();
+		}
+		catch(e)
+		{
+			xmlhttp=false;
+		}
+	}
+	/*if(!xmlhttp)
+	{
+		alert("Your browser doesn't seem to support XMLHttpRequests.");
+	}*/
+	return xmlhttp ;
+}
+
+function readFile(file)
+{
+	var rawFile = createXMLHttpObject() ;
+	rawFile.open("GET",file,true);
+	rawFile.onreadystatechange = function ()
+	{
+		if(rawFile.readyState == 4)
+		{
+			if(rawFile.status == 200 || rawFile.status == 0)
+			{
+				var allText = rawFile.responseText;
+				//alert(allText);
+			}
+		}
+	}
+	rawFile.send(null);
+	return rawFile;
+}
